bd7c3b024c98071cdf61334816cc5d793b156c9b
Xiawenlong-bug <2473833028@qq.com>
2024-02-15 11:47:39 +0800

    Frame

---
+++ src/Frame.cpp
@@ -0,0 +1,406 @@
+/*
+ * @Author: Xiawenlong-bug 2473833028@qq.com
+ * @Date: 2024-02-14 21:20:40
+ * @LastEditors: Xiawenlong-bug 2473833028@qq.com
+ * @LastEditTime: 2024-02-15 11:40:48
+ * @FilePath: /myORBSLAM2/src/Frame.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include "Frame.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <ctime>
+#include <thread>
+
+
+
+using namespace std;
+
+namespace ORB_SLAM2
+{
+    long unsigned int Frame::nNextId = 0;
+
+    bool Frame::mbInitialComputations=true; //initial
+
+
+float Frame::cx, Frame::cy, Frame::fx, Frame::fy, Frame::invfx, Frame::invfy;
+float Frame::mnMinX, Frame::mnMinY, Frame::mnMaxX, Frame::mnMaxY;
+float Frame::mfGridElementWidthInv, Frame::mfGridElementHeightInv;
+
+Frame::Frame(){}
+
+
+Frame::Frame(const Frame &frame)
+    :mpORBvocabulary(frame.mpORBvocabulary), 
+     mpORBextractorLeft(frame.mpORBextractorLeft), 
+     mpORBextractorRight(frame.mpORBextractorRight),
+     mTimeStamp(frame.mTimeStamp), 
+     mK(frame.mK.clone()),									//深拷贝
+     mDistCoef(frame.mDistCoef.clone()),					//深拷贝
+     mbf(frame.mbf), 
+     mb(frame.mb), 
+     mThDepth(frame.mThDepth), 
+     N(frame.N), 
+     mvKeys(frame.mvKeys),									//经过实验，确定这种通过同类型对象初始化的操作是具有深拷贝的效果的
+     mvKeysRight(frame.mvKeysRight), 						//深拷贝
+     mvKeysUn(frame.mvKeysUn),  							//深拷贝
+     mvuRight(frame.mvuRight),								//深拷贝
+     mvDepth(frame.mvDepth), 								//深拷贝
+     mBowVec(frame.mBowVec), 								//深拷贝
+     mFeatVec(frame.mFeatVec),								//深拷贝
+     mDescriptors(frame.mDescriptors.clone()), 				//cv::Mat深拷贝
+     mDescriptorsRight(frame.mDescriptorsRight.clone()),	//cv::Mat深拷贝
+     mvpMapPoints(frame.mvpMapPoints), 						//深拷贝
+     mvbOutlier(frame.mvbOutlier), 							//深拷贝
+     mnId(frame.mnId),
+     mpReferenceKF(frame.mpReferenceKF), 
+     mnScaleLevels(frame.mnScaleLevels),
+     mfScaleFactor(frame.mfScaleFactor), 
+     mfLogScaleFactor(frame.mfLogScaleFactor),
+     mvScaleFactors(frame.mvScaleFactors), 					//深拷贝
+     mvInvScaleFactors(frame.mvInvScaleFactors),			//深拷贝
+     mvLevelSigma2(frame.mvLevelSigma2), 					//深拷贝
+     mvInvLevelSigma2(frame.mvInvLevelSigma2)				//深拷贝
+{
+    	//逐个复制，其实这里也是深拷贝
+    for(int i=0;i<FRAME_GRID_COLS;i++)
+        for(int j=0; j<FRAME_GRID_ROWS; j++)
+			//这里没有使用前面的深拷贝方式的原因可能是mGrid是由若干vector类型对象组成的vector，
+			//但是自己不知道vector内部的源码不清楚其赋值方式，在第一维度上直接使用上面的方法可能会导致
+			//错误使用不合适的复制函数，导致第一维度的vector不能够被正确地“拷贝”
+            mGrid[i][j]=frame.mGrid[i][j];
+
+    if(!frame.mTcw.empty())
+		//这里说的是给新的帧设置Pose
+        SetPose(frame.mTcw);
+}
+    
+Frame::~Frame(){}
+
+
+/**
+ * @brief 为双目相机准备的构造函数
+ * 
+ * @param[in] imLeft            左目图像
+ * @param[in] imRight           右目图像
+ * @param[in] timeStamp         时间戳
+ * @param[in] extractorLeft     左目图像特征点提取器句柄
+ * @param[in] extractorRight    右目图像特征点提取器句柄
+ * @param[in] voc               ORB字典句柄
+ * @param[in] K                 相机内参矩阵
+ * @param[in] distCoef          相机去畸变参数
+ * @param[in] bf                相机基线长度和焦距的乘积
+ * @param[in] thDepth           远点和近点的深度区分阈值
+ *  
+ */
+Frame::Frame(const cv::Mat &imLeft, const cv::Mat &imRight, const double &timeStamp, ORBextractor* extractorLeft, ORBextractor* extractorRight, ORBVocabulary* voc, cv::Mat &K, cv::Mat &distCoef, const float &bf, const float &thDepth)
+    :mpORBvocabulary(voc),mpORBextractorLeft(extractorLeft),mpORBextractorRight(extractorRight), mTimeStamp(timeStamp), mK(K.clone()),mDistCoef(distCoef.clone()), mbf(bf), mThDepth(thDepth),
+     mpReferenceKF(static_cast<KeyFrame*>(NULL))
+{
+    // Step 1 帧的ID 自增
+    mnId=nNextId++;
+
+    // Step 2 计算图像金字塔的参数 
+	//获取图像金字塔的层数
+    mnScaleLevels = mpORBextractorLeft->GetLevels();
+	//这个是获得层与层之前的缩放比
+    mfScaleFactor = mpORBextractorLeft->GetScaleFactor();
+	//计算上面缩放比的对数, NOTICE log=自然对数，log10=才是以10为基底的对数 
+    mfLogScaleFactor = log(mfScaleFactor);
+	//获取每层图像的缩放因子
+    mvScaleFactors = mpORBextractorLeft->GetScaleFactors();
+	//同样获取每层图像缩放因子的倒数
+    mvInvScaleFactors = mpORBextractorLeft->GetInverseScaleFactors();
+	//高斯模糊的时候，使用的方差
+    mvLevelSigma2 = mpORBextractorLeft->GetScaleSigmaSquares();
+	//获取sigma^2的倒数
+    mvInvLevelSigma2 = mpORBextractorLeft->GetInverseScaleSigmaSquares();
+
+    // ORB extraction
+    // Step 3 对左目右目图像提取ORB特征点, 第一个参数0-左图， 1-右图。为加速计算，同时开了两个线程计算
+    thread threadLeft(&Frame::ExtractORB,		//该线程的主函数
+					  this,						//当前帧对象的对象指针
+					  0,						//表示是左图图像
+					  imLeft);					//图像数据
+	//对右目图像提取ORB特征，参数含义同上
+    thread threadRight(&Frame::ExtractORB,this,1,imRight);
+	//等待两张图像特征点提取过程完成
+    threadLeft.join();
+    threadRight.join();
+
+	//mvKeys中保存的是左图像中的特征点，这里是获取左侧图像中特征点的个数
+    N = mvKeys.size();
+
+	//如果左图像中没有成功提取到特征点那么就返回，也意味这这一帧的图像无法使用
+    if(mvKeys.empty())
+        return;
+	
+    // Step 4 用OpenCV的矫正函数、内参对提取到的特征点进行矫正
+    // 实际上由于双目输入的图像已经预先经过矫正,所以实际上并没有对特征点进行任何处理操作
+    UndistortKeyPoints();
+
+    // Step 5 计算双目间特征点的匹配，只有匹配成功的特征点会计算其深度,深度存放在 mvDepth 
+	// mvuRight中存储的应该是左图像中的点所匹配的在右图像中的点的横坐标（纵坐标相同）
+    ComputeStereoMatches();
+
+    // 初始化本帧的地图点
+    mvpMapPoints = vector<MapPoint*>(N,static_cast<MapPoint*>(NULL));   
+	// 记录地图点是否为外点，初始化均为外点false
+    mvbOutlier = vector<bool>(N,false);
+
+    // This is done only for the first Frame (or after a change in the calibration)
+	//  Step 5 计算去畸变后图像边界，将特征点分配到网格中。这个过程一般是在第一帧或者是相机标定参数发生变化之后进行
+    if(mbInitialComputations)
+    {
+		//计算去畸变后图像的边界
+        ComputeImageBounds(imLeft);
+
+		// 表示一个图像像素相当于多少个图像网格列（宽）
+        mfGridElementWidthInv=static_cast<float>(FRAME_GRID_COLS)/static_cast<float>(mnMaxX-mnMinX);
+		// 表示一个图像像素相当于多少个图像网格行（高）
+        mfGridElementHeightInv=static_cast<float>(FRAME_GRID_ROWS)/static_cast<float>(mnMaxY-mnMinY);
+
+		//给类的静态成员变量复制
+        fx = K.at<float>(0,0);
+        fy = K.at<float>(1,1);
+        cx = K.at<float>(0,2);
+        cy = K.at<float>(1,2);
+		// 猜测是因为这种除法计算需要的时间略长，所以这里直接存储了这个中间计算结果
+        invfx = 1.0f/fx;
+        invfy = 1.0f/fy;
+
+		//特殊的初始化过程完成，标志复位
+        mbInitialComputations=false;
+    }
+
+    // 双目相机基线长度
+    mb = mbf/fx;
+
+    // 将特征点分配到图像网格中 
+    AssignFeaturesToGrid();    
+}
+
+/**
+ * @brief 为RGBD相机准备的帧构造函数
+ * 
+ * @param[in] imGray        对RGB图像灰度化之后得到的灰度图像
+ * @param[in] imDepth       深度图像
+ * @param[in] timeStamp     时间戳
+ * @param[in] extractor     特征点提取器句柄
+ * @param[in] voc           ORB特征点词典的句柄
+ * @param[in] K             相机的内参数矩阵
+ * @param[in] distCoef      相机的去畸变参数
+ * @param[in] bf            baseline*bf
+ * @param[in] thDepth       远点和近点的深度区分阈值
+ */
+Frame::Frame(const cv::Mat &imGray, const cv::Mat &imDepth, const double &timeStamp, ORBextractor* extractor,ORBVocabulary* voc, cv::Mat &K, cv::Mat &distCoef, const float &bf, const float &thDepth)
+    :mpORBvocabulary(voc),mpORBextractorLeft(extractor),mpORBextractorRight(static_cast<ORBextractor*>(NULL)),
+     mTimeStamp(timeStamp), mK(K.clone()),mDistCoef(distCoef.clone()), mbf(bf), mThDepth(thDepth)
+{
+    // Step 1 帧的ID 自增
+    mnId=nNextId++;
+
+    // Step 2 计算图像金字塔的参数 
+	//获取图像金字塔的层数
+    mnScaleLevels = mpORBextractorLeft->GetLevels();
+	//获取每层的缩放因子
+    mfScaleFactor = mpORBextractorLeft->GetScaleFactor();    
+	//计算每层缩放因子的自然对数
+    mfLogScaleFactor = log(mfScaleFactor);
+	//获取各层图像的缩放因子
+    mvScaleFactors = mpORBextractorLeft->GetScaleFactors();
+	//获取各层图像的缩放因子的倒数
+    mvInvScaleFactors = mpORBextractorLeft->GetInverseScaleFactors();
+	//TODO 也是获取这个不知道有什么实际含义的sigma^2
+    mvLevelSigma2 = mpORBextractorLeft->GetScaleSigmaSquares();
+	//计算上面获取的sigma^2的倒数
+    mvInvLevelSigma2 = mpORBextractorLeft->GetInverseScaleSigmaSquares();
+
+    /** 3. 提取彩色图像(其实现在已经灰度化成为灰度图像了)的特征点 \n Frame::ExtractORB() */
+
+    // ORB extraction
+	// Step 3 对图像进行提取特征点, 第一个参数0-左图， 1-右图
+    ExtractORB(0,imGray);
+
+	//获取特征点的个数
+    N = mvKeys.size();
+
+	//如果这一帧没有能够提取出特征点，那么就直接返回了
+    if(mvKeys.empty())
+        return;
+
+	// Step 4 用OpenCV的矫正函数、内参对提取到的特征点进行矫正
+    UndistortKeyPoints();
+
+	// Step 5 获取图像的深度，并且根据这个深度推算其右图中匹配的特征点的视差
+    ComputeStereoFromRGBD(imDepth);
+
+    // 初始化本帧的地图点
+    mvpMapPoints = vector<MapPoint*>(N,static_cast<MapPoint*>(NULL));
+	// 记录地图点是否为外点，初始化均为外点false
+    mvbOutlier = vector<bool>(N,false);
+
+    // This is done only for the first Frame (or after a change in the calibration)
+	//  Step 5 计算去畸变后图像边界，将特征点分配到网格中。这个过程一般是在第一帧或者是相机标定参数发生变化之后进行
+    if(mbInitialComputations)
+    {
+		//计算去畸变后图像的边界
+        ComputeImageBounds(imGray);
+
+        // 表示一个图像像素相当于多少个图像网格列（宽）
+        mfGridElementWidthInv=static_cast<float>(FRAME_GRID_COLS)/static_cast<float>(mnMaxX-mnMinX);
+		// 表示一个图像像素相当于多少个图像网格行（高）
+        mfGridElementHeightInv=static_cast<float>(FRAME_GRID_ROWS)/static_cast<float>(mnMaxY-mnMinY);
+
+		//给类的静态成员变量复制
+        fx = K.at<float>(0,0);
+        fy = K.at<float>(1,1);
+        cx = K.at<float>(0,2);
+        cy = K.at<float>(1,2);
+		// 猜测是因为这种除法计算需要的时间略长，所以这里直接存储了这个中间计算结果
+        invfx = 1.0f/fx;
+        invfy = 1.0f/fy;
+
+		//特殊的初始化过程完成，标志复位
+        mbInitialComputations=false;
+    }
+
+    // 计算假想的基线长度 baseline= mbf/fx
+    // 后面要对从RGBD相机输入的特征点,结合相机基线长度,焦距,以及点的深度等信息来计算其在假想的"右侧图像"上的匹配点
+    mb = mbf/fx;
+
+	// 将特征点分配到图像网格中 
+    AssignFeaturesToGrid();
+}
+
+/**
+ * @brief 单目帧构造函数
+ * 
+ * @param[in] imGray                            //灰度图
+ * @param[in] timeStamp                         //时间戳
+ * @param[in & out] extractor                   //ORB特征点提取器的句柄
+ * @param[in] voc                               //ORB字典的句柄
+ * @param[in] K                                 //相机的内参数矩阵
+ * @param[in] distCoef                          //相机的去畸变参数
+ * @param[in] bf                                //baseline*f
+ * @param[in] thDepth                           //区分远近点的深度阈值
+ */
+Frame::Frame(const cv::Mat &imGray, const double &timeStamp, ORBextractor* extractor,ORBVocabulary* voc, cv::Mat &K, cv::Mat &distCoef, const float &bf, const float &thDepth)
+    :mpORBvocabulary(voc),mpORBextractorLeft(extractor),mpORBextractorRight(static_cast<ORBextractor*>(NULL)),
+     mTimeStamp(timeStamp), mK(K.clone()), mDistCoef(distCoef.clone()), mbf(bf), mThDepth(thDepth)
+{
+    // Frame ID
+	// Step 1 帧的ID 自增
+    mnId=nNextId++;
+
+    // Step 2 计算图像金字塔的参数 
+    // Scale Level Info
+	//获取图像金字塔的层数
+    mnScaleLevels = mpORBextractorLeft->GetLevels();
+	//获取每层的缩放因子
+    mfScaleFactor = mpORBextractorLeft->GetScaleFactor();
+	//计算每层缩放因子的自然对数
+    mfLogScaleFactor = log(mfScaleFactor);
+	//获取各层图像的缩放因子
+    mvScaleFactors = mpORBextractorLeft->GetScaleFactors();
+	//获取各层图像的缩放因子的倒数
+    mvInvScaleFactors = mpORBextractorLeft->GetInverseScaleFactors();
+	//获取sigma^2
+    mvLevelSigma2 = mpORBextractorLeft->GetScaleSigmaSquares();
+	//获取sigma^2的倒数
+    mvInvLevelSigma2 = mpORBextractorLeft->GetInverseScaleSigmaSquares();
+
+    // ORB extraction
+	// Step 3 对这个单目图像进行提取特征点, 第一个参数0-左图， 1-右图
+    ExtractORB(0,imGray);
+
+	//求出特征点的个数
+    N = mvKeys.size();
+
+	//如果没有能够成功提取出特征点，那么就直接返回了
+    if(mvKeys.empty())
+        return;
+
+    // Step 4 用OpenCV的矫正函数、内参对提取到的特征点进行矫正 
+    UndistortKeyPoints();
+
+    // Set no stereo information
+	// 由于单目相机无法直接获得立体信息，所以这里要给右图像对应点和深度赋值-1表示没有相关信息
+    mvuRight = vector<float>(N,-1);
+    mvDepth = vector<float>(N,-1);
+
+
+    // 初始化本帧的地图点
+    mvpMapPoints = vector<MapPoint*>(N,static_cast<MapPoint*>(NULL));
+	// 记录地图点是否为外点，初始化均为外点false
+    mvbOutlier = vector<bool>(N,false);
+
+    // This is done only for the first Frame (or after a change in the calibration)
+	//  Step 5 计算去畸变后图像边界，将特征点分配到网格中。这个过程一般是在第一帧或者是相机标定参数发生变化之后进行
+    if(mbInitialComputations)
+    {
+		// 计算去畸变后图像的边界
+        ComputeImageBounds(imGray);
+
+		// 表示一个图像像素相当于多少个图像网格列（宽）
+        mfGridElementWidthInv=static_cast<float>(FRAME_GRID_COLS)/static_cast<float>(mnMaxX-mnMinX);
+		// 表示一个图像像素相当于多少个图像网格行（高）
+        mfGridElementHeightInv=static_cast<float>(FRAME_GRID_ROWS)/static_cast<float>(mnMaxY-mnMinY);
+
+		//给类的静态成员变量复制
+        fx = K.at<float>(0,0);
+        fy = K.at<float>(1,1);
+        cx = K.at<float>(0,2);
+        cy = K.at<float>(1,2);
+		// 猜测是因为这种除法计算需要的时间略长，所以这里直接存储了这个中间计算结果
+        invfx = 1.0f/fx;
+        invfy = 1.0f/fy;
+
+		//特殊的初始化过程完成，标志复位
+        mbInitialComputations=false;
+    }
+
+    //计算 basline
+    mb = mbf/fx;
+
+	// 将特征点分配到图像网格中 
+    AssignFeaturesToGrid();
+}
+
+/**
+ * @brief 将提取的ORB特征点分配到图像网格中
+ * 
+ */
+void Frame::AssignFeaturesToGrid()
+{
+    // Step 1  给存储特征点的网格数组 Frame::mGrid 预分配空间
+	// ? 这里0.5 是为什么？节省空间？
+    // FRAME_GRID_COLS = 64，FRAME_GRID_ROWS=48
+    int nReserve = 0.5f*N/(FRAME_GRID_COLS*FRAME_GRID_ROWS);
+	//开始对mGrid这个二维数组中的每一个vector元素遍历并预分配空间
+    for(unsigned int i=0; i<FRAME_GRID_COLS;i++)
+        for (unsigned int j=0; j<FRAME_GRID_ROWS;j++)
+            mGrid[i][j].reserve(nReserve);
+
+    // Step 2 遍历每个特征点，将每个特征点在mvKeysUn中的索引值放到对应的网格mGrid中
+    for(int i=0;i<N;i++)
+    {
+		//从类的成员变量中获取已经去畸变后的特征点
+        const cv::KeyPoint &kp = mvKeysUn[i];
+
+		//存储某个特征点所在网格的网格坐标，nGridPosX范围：[0,FRAME_GRID_COLS], nGridPosY范围：[0,FRAME_GRID_ROWS]
+        int nGridPosX, nGridPosY;
+		// 计算某个特征点所在网格的网格坐标，如果找到特征点所在的网格坐标，记录在nGridPosX,nGridPosY里，返回true，没找到返回false
+        if(PosInGrid(kp,nGridPosX,nGridPosY))
+			//如果找到特征点所在网格坐标，将这个特征点的索引添加到对应网格的数组mGrid中
+            mGrid[nGridPosX][nGridPosY].push_back(i);
+    }
+}
+
+
+}
